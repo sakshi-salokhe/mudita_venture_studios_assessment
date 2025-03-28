610547509dd701a6ce137d24a38ae92e987820a3
sakshi-salokhe <sakshisalokhe1@gmail.com>
2025-03-28 13:01:48 -0500

    fe base page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <BasePage />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BasePage from './components/BasePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BasePage
   }
 }
 </script>
 
-<style>
-#app {
+<style scoped>
+
+html, body, #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  margin: auto;
+  background-color: #2c3e50;
+  width: 97%;
+  height: 47.5vw;
+  color: #f2f2f2;
+
+  padding: 1vw;
 }
 </style>
---
+++ frontend/src/components/BasePage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="base-page-wrapper">
+        <div class="base-title">
+            Schedule Builder
+        </div>
+
+        <div class="base-body">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BasePage'
+}
+</script>
+
+<style scoped>
+    .base-page-wrapper {
+        height: 100%;
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .base-title {
+        height: 5%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        font-size: 1.25vw;
+        font-weight: 700;
+        text-transform: uppercase;
+    }
+
+    .base-body {
+        height: 93%;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        border: 1px solid red;
+    }
+</style>
