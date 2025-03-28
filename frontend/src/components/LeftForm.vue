e1b916fa890c1be1633f8eff55fbe47f8cce2df5
sakshi-salokhe <sakshisalokhe1@gmail.com>
2025-03-28 13:34:17 -0500

    input

--- frontend/src/components/LeftForm.vue
+++ frontend/src/components/LeftForm.vue
@@ -5,11 +5,20 @@
                 Enter activity details
             </div>
 
-            <button class="create-schedule-button">Create Schedule</button>
+            <button class="create-schedule-button" >
+                Create Schedule
+            </button>
         </div>
 
         <div class="left-form-body">
+            <div class="input-wrapper">
+                <textarea rows="5" placeholder="Add activity here" />
+                <button class="add-button">Add</button>
+            </div>
 
+            <div class="items-body">
+
+            </div>
         </div>
     </div>
 </template>
@@ -70,10 +79,66 @@ export default {
         text-transform: uppercase;
         font-weight: 700;
     }
+    .create-schedule-button:disabled {
+        background-color: #575c62;
+        border: 1px solid #575c62;
+    }
 
     .left-form-body {
         height: 88%;
-
         padding: 0.5vw;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .input-wrapper {
+        height: 9%;
+        width: 97%;
+        padding: 0.5vw;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    textarea {
+        background-color: inherit;
+        border: 1px solid #2c3e50;
+        outline: none;
+
+        width: 75%;
+    }
+    textarea::placeholder{
+        color: #c2c2c2;
+        padding: 0.25vw;
+    }
+
+    .add-button {
+        height: max-content;
+        width: 20%;
+        padding: 0.4vw 1vw;
+        border-radius: 0.25vw;
+
+        background-color: #2c3e50;
+        border: 1px solid #2c3e50;
+
+        color: #f2f2f2;
+        text-transform: uppercase;
+        font-weight: 700;
+    }
+    .add-button:disabled {
+        background-color: #575c62;
+        border: 1px solid #575c62;
+    }
+
+    .items-body {
+        height: 82%;
+        width: 97%;
+        padding: 0.5vw;
+
+        overflow-y: auto;
     }
 </style>
